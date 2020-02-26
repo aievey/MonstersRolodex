3247ee0c796b623a50f0da760d07183e7c724aff
Amitabh Verma <amitabh283verma@gmail.com>
2020-02-26 06:42:11 +0530

    Updates

---
+++ precache-manifest.330910d82d3c279549ec34a712e3ca20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7038ddc297b60af9b63c4d0dc3732b90",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "94406d83dd887db06a38",
+    "url": "/MonstersRolodex/static/css/main.54a135e7.chunk.css"
+  },
+  {
+    "revision": "5e79c65f0330fd85d8cc",
+    "url": "/MonstersRolodex/static/js/2.e6b78da9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/MonstersRolodex/static/js/2.e6b78da9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94406d83dd887db06a38",
+    "url": "/MonstersRolodex/static/js/main.ba9f232b.chunk.js"
+  },
+  {
+    "revision": "cfa10c67121f5469363c",
+    "url": "/MonstersRolodex/static/js/runtime-main.91a7d740.js"
+  }
+]);
